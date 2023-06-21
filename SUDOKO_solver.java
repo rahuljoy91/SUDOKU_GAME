abf8d19a4d4d4cc3ba18eb00b23aeef1bbecd9b9
rahuljoy91 <137314948+rahuljoy91@users.noreply.github.com>
2023-06-21 22:42:43 +0530

    Add files via upload

---
+++ SUDOKO_solver.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sudoko_solver;
+
+/**
+ *
+ * @author kutti
+ */
+public class SUDOKO_solver {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
